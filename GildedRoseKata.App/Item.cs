d2d4735b0e630a2b85169f17c686f625d9cbe637
Alvaro Atias <aloatias@hotmail.com>
2020-08-25 17:53:53 +0200

    Add project files.

---
+++ GildedRoseKata.App/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace GildedRoseKata.App.Models
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+    }
+}
---
+++ GildedRoseKata.Tests/GildedRoseTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata.App.Models;
+using GildedRoseKata.App.Core;
+
+namespace GildedRoseKata.Tests
+{
+    public class GildedRoseTests
+    {
+        [Fact]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal("foo", Items[0].Name);
+        }
+    }
+}
